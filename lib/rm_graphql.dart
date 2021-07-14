157ca7b9798d46670f9bb5e808bb061204cded99
Joseph Ikinda <josephikinda@gmail.com>
2021-07-14 19:56:29 +0200

    third commit

--- lib/rm_graphql.dart
+++ lib/rm_graphql.dart
@@ -1,6 +1,10 @@
 import 'package:ferry/ferry.dart';
 import 'package:gql_http_link/gql_http_link.dart';
 
+export 'src/operation.data.gql.dart';
+export 'src/operation.var.gql.dart';
+export 'src/operation.req.gql.dart';
+
 Client initClient(String url, String secret) {
   final link = HttpLink(url, defaultHeaders: {"x-hasura-admin-secret": secret});
 
